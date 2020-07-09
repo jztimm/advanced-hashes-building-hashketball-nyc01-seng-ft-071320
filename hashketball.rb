c6863c1f2e604809c8df9baf38d318f8bbe1c219
Jzavier Timm <jztimm@gmail.com>
2020-07-09 20:42:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,50 @@
 def game_hash
   {
     home: {
+      team_name: "",
+      colors: [],
+      players: {
+        [ 
+         player_name:,
+         number:,
+         shoe:,
+         points:,
+         rebounds:,
+         assists:,
+         steals:,
+         blocks:,
+         slam_dunks:
+        }
+        ]
+    },
+    away: {
+      team_name: "",
+      colors: [],
+      players: [
+        {
+         player_name:,
+         number:,
+         shoe:,
+         points:,
+         rebounds:,
+         assists:,
+         steals:,
+         blocks:,
+         slam_dunks:
+        }
+        ]
+    }
+  }
+end
+
+
+
+
+
+
+def game_hash
+  {
+    home: {
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
       players: [
